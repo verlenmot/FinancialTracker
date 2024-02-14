13caa9b31897f681aa72b733cfeca2dc89b6499e
Lennert Vermote <lennert.vermote@linkit.nl>
2024-02-14 17:52:44 +0100

    feat: expand case class Pool

--- FinancialTracker/src/main/scala/baseClasses/Pool.scala
+++ FinancialTracker/src/main/scala/baseClasses/Pool.scala
@@ -1,29 +1,33 @@
 package baseClasses
 
+import scala.annotation.tailrec
 import scala.reflect.ClassTag
 
-case class Pool[A <: Financials: ClassTag](private val name: String, private val description: String,
-                                           private val pool: Array[A], private val active: Boolean) {
+case class Pool[A <: Financials](private val name: String, private val description: String,
+                                 private val pool: List[A], private val active: Boolean) {
   def getName: String = name
   def getDescription: String = description
   def setName(newName: String): Pool[A] = this.copy(name = newName)
   def setDescription(newDescription: String): Pool[A] = this.copy(description = newDescription)
-  def addItem(item: A): Pool[A] = this.copy(pool = pool :+ item)
-  def removeItem(item: A): Pool[A] = this.copy(pool = pool.filter(_ == item))
-  def listItems(): String = pool.mkString(",")
-  def total(): Double = {
-    def tailTotal(n: Int = 0, accumulator: Double = 0): Double = {
+  def addFinancials(item: A): Pool[A] = this.copy(pool = pool :+ item)
+  def removeFinancials(item: A): Pool[A] = this.copy(pool = pool.filter(_ == item))
+  def poolTotal(): Double = {
+    @tailrec
+    def poolTotalTail(n: Int = 0, accumulator: Double = 0): Double = {
       if (n == pool.length) accumulator
-      else tailTotal(n + 1, accumulator + pool(n).getBalance)
+      else poolTotalTail(n + 1, accumulator + pool(n).getBalance)
     }
-    tailTotal()
+
+    poolTotalTail()
   }
-  def detailedListing(): String = {
-    def tailDetailed(n: Int = 0, accumulator: String = s"Pool: ${this.getName} - "): String = {
+  def listFinancials(): String = pool.mkString(",")
+  def describeFinancials(): String = {
+    @tailrec
+    def describeFinancialsTail(n: Int = 0, accumulator: String = s"Pool: ${this.getName} - "): String = {
       if (n == pool.length) accumulator
-      else tailDetailed(n + 1, accumulator + s"Name: ${pool(n).getName} - Balance: ${pool(n).getBalance}\n")
+      else describeFinancialsTail(n + 1, accumulator + s"Name: ${pool(n).getName} - Balance: ${pool(n).getBalance}\n")
     }
-    tailDetailed()
+    describeFinancialsTail()
   }
-  def apply(): String = s"Type: ${this.getClass}\n Name: $name\n Description: $description\n Items: ${this.listItems}\n Active: $active"
+  def apply(): String = s"Type: ${this.getClass}\n Name: $name\n Description: $description\n Financials: ${this.listFinancials()}\n Active: $active"
 }
